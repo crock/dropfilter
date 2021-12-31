aec4754f971973b537ca671c64c0164862804332
Alex Crocker <croc@hey.com>
2021-12-31 13:55:04 -0500

    Added cors headers

--- pages/api/backorderServices.ts
+++ pages/api/backorderServices.ts
@@ -5,5 +5,6 @@ export default (req: NextApiRequest, res: NextApiResponse) => {
 
     const filtered = data.filter(s => s.active)
 
+    res.setHeader('Access-Control-Allow-Origin', '*')
     res.status(200).json(filtered)
 }
